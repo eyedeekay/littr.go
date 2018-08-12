ab2237ccd259f7efbf8584b34cd5005c06b6800e
Marius Orcsik <marius.orcsik@rocket-internet.de>
2018-08-12 13:24:35 +0200

    Added nginx

--- rkt/build-nginx.sh
+++ rkt/build-nginx.sh
@@ -1,50 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$EUID" -ne 0 ]; then
-    echo "This script uses functionality which requires root privileges"
-    exit 1
-fi
-
-# Start the build with an empty ACI
-acbuild --debug begin
-
-# In the event of the script exiting, end the build
-acbuildEnd() {
-    export EXIT=$?
-    acbuild --debug end && exit $EXIT
-}
-trap acbuildEnd EXIT
+# Start the build from alpine-os
+newcontainer=$(buildah --debug --registries-conf ./registries.toml from quay.io/coreos/alpine-sh )
 
 __version=$(printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
 
-# Name the ACI
-acbuild --debug set-name littr.me/nginx
-
 # Based on alpine
-acbuild --debug dep add quay.io/coreos/alpine-sh
+buildah --debug config --arch amd64 ${newcontainer}
+buildah --debug config --os linux ${newcontainer}
 
-acbuild --debug label add arch amd64
-acbuild --debug label add os linux
-acbuild --debug label add version "${__version}"
+# Install postgres
+buildah --debug run ${newcontainer} -- apk update
+buildah --debug run ${newcontainer} -- apk add nginx
 
-# Install nginx
-acbuild --debug run apk update
-acbuild --debug run apk add nginx
+buildah --debug run ${newcontainer} -- bash -c 'echo "nginx on OCI alpine os image, built using Buildah" > /usr/share/nginx/html/index.html'
+buildah --debug config --port 80 --entrypoint /usr/sbin/nginx ${newcontainer}
 
-acbuild copy rkt/nginx/nginx.conf /etc/nginx/nginx.conf
+buildah --debug run copy ${newcontainer} ../nginx/nginx.conf /etc/nginx/nginx.conf
 
-# Add a port for http traffic over port 80
-acbuild --debug port add http tcp 80
+buildah --debug config --created-by "Marius Orcsik <marius@littr.me>" ${newcontainer}
+buildah --debug config --author "lpbm" ${newcontainer}
 
-# Add a mount point for files to serve
-#acbuild --debug mount add html /usr/share/nginx/html
-
-# Run nginx in the foreground
-acbuild --debug set-exec -- /usr/sbin/nginx -g "daemon off;"
-
-acbuild annotation add authors "Marius Orcsik <marius@littr.me>"
-
-# Save the ACI
-acbuild --debug write --overwrite "littr-me-nginx-${__version}-linux-amd64.aci"
-
+image="littr-me-nginx-${__version}-linux-amd64"
+# Save the image
+buildah --debug inspect ${newcontainer}
+buildah --debug commit --rm --signature-policy ./insecureAcceptAnything.json --squash ${newcontainer} ${image}
+buildah --debug push --format oci --signature-policy ./insecureAcceptAnything.json "${image}" "oci-archive:./${image}.oci"
